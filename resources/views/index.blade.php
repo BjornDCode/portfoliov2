f20e24d3a2501028814f3db4d5a811449ecba0ab
Asbjørn <asbjorn.lindholm@hotmail.com>
2017-05-08 22:24:13 +0200

    Added stylesheet

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,10 @@
         <meta charset="utf-8">
         <title>{{ config('app.name', 'Bjørn Lindholm') }}</title>
 
-        {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     </head>
     <body>
-
+        <h1>{{ config('app.name', 'Bjørn Lindholm') }}</h1>
     </body>
 </html>
