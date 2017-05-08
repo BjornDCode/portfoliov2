cd141ef4200a656f6ee1656489afb43149dbb186
Asbjørn <asbjorn.lindholm@hotmail.com>
2017-05-08 22:04:07 +0200

    Index blade file initial setup

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>{{ config('app.name', 'Bjørn Lindholm') }}</title>
+
+        {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+
+    </head>
+    <body>
+
+    </body>
+</html>
