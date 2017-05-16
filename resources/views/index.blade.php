ca975cbdc8e023b6d2a93d173a9d0ae36856948c
BjornDCode <bjorn@lindholmhansen.com>
2017-05-16 16:20:36 +0200

    Include fonts from typekit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8">
         <title>{{ config('app.name', 'Bjørn Lindholm') }}</title>
 
+        <script src="https://use.typekit.net/jpv1qlz.js"></script>
+        <script>try{Typekit.load({ async: true });}catch(e){}</script>
+
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     </head>
